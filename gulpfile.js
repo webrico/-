0c09f32ba027faccd0dbfe730168c52853cb976a
web-rico <webdevrico@gmail.com>
2017-04-28 10:07:26 +0300

    fix smart-greed in start gulp

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ var lessCleanCss = require('less-plugin-clean-css');
 var autoprefixPlugin = new lessAutoPrefix({browsers: ["last 2 versions"]});
 var cleanCssPlugin = new lessCleanCss({sourceMaps: true});
 var gcmq = require('gulp-group-css-media-queries');
-var smartgrid = require('smart-grid');
+// var smartgrid = require('smart-grid');
 
 
 
@@ -85,42 +85,42 @@ gulp.task('default', function(){
 //start smart-grid
  
 /* It's principal settings in smart grid project */
-var settings = {
-    outputStyle: 'less', /* less || scss || sass || styl */
-    columns: 12, /* number of grid columns */
-    offset: "0px", /* gutter width px || % */
-    container: {
-        maxWidth: '1200px', /* max-width оn very large screen */
-        fields: '30px' /* side fields */
-    },
-    breakPoints: {
-        lg: {
-            'width': '1100px', /* -> @media (max-width: 1100px) */
-            'fields': '30px' /* side fields */
-        },
-        md: {
-            'width': '960px',
-            'fields': '15px'
-        },
-        sm: {
-            'width': '780px',
-            'fields': '15px'
-        },
-        xs: {
-            'width': '320px',
-            'fields': '20px'
-        }
-        /* 
-        We can create any quantity of break points.
+// var settings = {
+//     outputStyle: 'less', /* less || scss || sass || styl */
+//     columns: 12, /* number of grid columns */
+//     offset: "0px", /* gutter width px || % */
+//     container: {
+//         maxWidth: '1200px', /* max-width оn very large screen */
+//         fields: '30px' /* side fields */
+//     },
+//     breakPoints: {
+//         lg: {
+//             'width': '1100px', /* -> @media (max-width: 1100px) */
+//             'fields': '30px' /* side fields */
+//         },
+//         md: {
+//             'width': '960px',
+//             'fields': '15px'
+//         },
+//         sm: {
+//             'width': '780px',
+//             'fields': '15px'
+//         },
+//         xs: {
+//             'width': '320px',
+//             'fields': '20px'
+//         }
+//         /* 
+//         We can create any quantity of break points.
  
-        some_name: {
-            some_width: 'Npx',
-            some_offset: 'N(px|%)'
-        }
-        */
-    }
-};
+//         some_name: {
+//             some_width: 'Npx',
+//             some_offset: 'N(px|%)'
+//         }
+//         */
+//     }
+// };
  
-smartgrid('./dev/less/import', settings);
+// smartgrid('./dev/less/import', settings);
 
 //end smart-grid
